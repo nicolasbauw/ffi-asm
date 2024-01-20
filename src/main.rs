0c357cc8b99a4f4181f594e3171dd4092b980ad0
Nicolas BAUW <nbauw@hotmail.com>
2024-01-20 20:30:23 +0100

    cfg for aarch64 or arm

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,11 @@
 use core::arch::global_asm;
 
-global_asm!(include_str!("addition.s"));
+#[cfg(target_arch = "aarch64")]
+global_asm!(include_str!("aarch64/addition.s"));
+
+#[cfg(target_arch = "arm")]
+global_asm!(include_str!("armv6/addition.s"));
+
 extern "C" {
     fn add_numbers(a: i32, b: i32) -> i32;
 }
